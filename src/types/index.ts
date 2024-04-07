078e0ba89b628cc7474073859669e053f04ecb5c
Raysuner <1669232805@qq.com>
2024-04-07 22:53:01 +0800

    feat:增加类型

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,9 @@
+export interface IResultDto<T> {
+  code: number;
+  success: boolean;
+  data: T;
+}
+
 export interface ILoginParams {
   username: string;
   password: string;
@@ -8,13 +14,15 @@ export interface IRegisterParams extends ILoginParams {
   captcha: string;
 }
 
-export interface IResultDto<T> {
-  code: number;
-  success: boolean;
-  data: T;
-}
-
 export interface ILoginResult {
   accessToken: string;
   refreshToken: string;
 }
+
+export interface IUpdatePassword {
+  password: string;
+  email: string;
+  captcha: string;
+}
+
+export interface IRefreshTokenResult extends ILoginResult {}
