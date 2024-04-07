a873e9312f73a6490dbcba3642fa9ab3babe1d1d
Raysuner <1669232805@qq.com>
2024-04-07 22:52:47 +0800

    feat: 处理双token刷新问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,25 +1,53 @@
 import axios from "axios";
+import { refreshToken } from "../api";
+import { message } from "antd";
 
 const request = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 2000,
 });
 
-// request.interceptors.request.use(
-//   (config) => {
-//     config.headers.setAuthorization(localStorage.getItem("accessToken"));
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+request.interceptors.request.use(
+  (config) => {
+    let token = "";
+    if (config.url?.includes?.("refreshToken")) {
+      token = localStorage.getItem("refreshToken") || "";
+    } else {
+      token = localStorage.getItem("accessToken") || "";
+    }
+    if (token) {
+      config.headers.setAuthorization(`Bearer ${token}`);
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 request.interceptors.response.use(
   (res) => {
     return res;
   },
-  (error) => {
-    const msg = error.response.data;
-    return Promise.reject(msg);
+  async (error) => {
+    const { data, config } = error.response;
+    if (data.code === 10011 && !config.url.includes("/user/refreshToken")) {
+      const res = await refreshToken();
+      if (res.success) {
+        localStorage.setItem("accessToken", res.data.accessToken);
+        localStorage.setItem("refreshToken", res.data.refreshToken);
+        return request({
+          method: config.method,
+          url: config.url,
+          params: config.data,
+          data: config.data,
+        });
+      } else {
+        message.error("登录态失效，请重新登录");
+        setTimeout(() => {
+          location.href = "/login";
+        });
+      }
+    }
+    return Promise.reject(data);
   }
 );
 
